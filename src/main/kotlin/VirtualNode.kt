4afd361e71b9991a8a50d2328d91af2905c4674d
MariaMsu <i.knaz@yandex.ru>
2023-01-17 10:39:31 +0100

    add pseudocode

---
+++ src/main/kotlin/VirtualNode.kt
@@ -0,0 +1,25 @@
+class VirtualNode(
+    val nodeNumber: Int, totalNodesNumber: Int,
+    duplicatFactor: Int, node2address: Map<Int, String>,
+    data: Map<String, String>
+) {
+    fun runHeartbeat() {
+        // todo
+    }
+
+    fun catchHeartbeat() {
+        // todo
+    }
+
+    fun setLongUrl(longUrl: String) {
+        // todo
+    }
+
+    fun duplicateLongUrl(longUrl: String) {
+        // todo
+    }
+
+    fun getLongUrl(shortUrl: Int) {
+        // todo
+    }
+}
